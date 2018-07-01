e0f3e84262802a522cd72fb8868bea15c7866b59
Dien Luong <dysonlu@gmail.com>
2018-06-30 21:38:16 -0400

    Implemented add-node button; node rename; active node indicator

--- client_src/src/components/NotesList.js
+++ client_src/src/components/NotesList.js
@@ -1,23 +1,26 @@
 // TODO: Delete Note.js
 // import Note from './Note';
 
-import Tree, { removeNode } from 'react-sortable-tree';
+import Tree, { removeNode, changeNodeAtPath, addNodeUnderParent } from 'react-sortable-tree';
 import 'react-sortable-tree/style.css';
-// TODO: Delete NotesList.css
-// import './NotesList.css';
+import './NotesList.css';
 import React from 'react';
+import uniqid from 'uniqid';
 
 import sampleNotes from '../test/sample-tree';
+
+const getNodeKey = ({ treeIndex }) => treeIndex;
 
 class NotesList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       notesTree: sampleNotes,
+      activeNote: null,
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.deleteNote = this.deleteNote.bind(this);
+    this.buildNodeProps = this.buildNodeProps.bind(this);
   }
 
   handleChange(notesTree) {
@@ -26,36 +29,97 @@ class NotesList extends React.Component {
     });
   }
 
-  deleteNote({ path }) {
-    return ({
-      buttons: [
+  handleNodeClick(node) {
+    console.log(`Active node: ${node.title} ${node.id}`);
+    this.setState({
+      activeNote: node.id,
+    });
+  }
+
+  _buildNodeButtons({ node, path }) {
+    let buttons = [
+      <button
+        style={{ verticalAlign: 'middle' }}
+        onClick={ (event) => {
+          event.stopPropagation();
+          const activeNote = (node.id === this.state.activeNote) ? null : this.state.activeNote;
+          const { treeData } = removeNode({
+            treeData: this.state.notesTree,
+            getNodeKey,
+            path,
+          });
+          this.setState({
+            notesTree: treeData,
+            activeNote,
+          });
+        }}
+      >
+        x
+      </button>,
+    ];
+
+    // Check if current node is parent node
+    if (typeof node.children !== 'undefined') {
+      buttons.unshift(
         <button
           style={{ verticalAlign: 'middle' }}
-          onClick={() => {
-            const { treeData } = removeNode({
+          onClick={ (event) => {
+            event.stopPropagation();
+            const { treeData } = addNodeUnderParent({
               treeData: this.state.notesTree,
-              getNodeKey: ({ treeIndex }) => treeIndex,
-              path,
+              getNodeKey,
+              parentKey: path[path.length - 1],
+              newNode: {
+                title: 'new note',
+                subtitle: () => new Date().toLocaleString(),
+                id: uniqid(),
+              },
+              expandParent: true,
             });
             this.setState({
               notesTree: treeData,
             });
           }}
         >
-          x
-        </button>,
-      ],
+          +
+        </button>);
+    }
+
+    return buttons;
+  }
+
+  buildNodeProps({ node, path }) {
+    return ({
+      title: (
+        <input
+          value={ node.title }
+          onChange={ event => {
+            const title = event.target.value;
+            this.setState({
+              notesTree: changeNodeAtPath({
+                treeData: this.state.notesTree,
+                path,
+                newNode: { ...node, title },
+                getNodeKey,
+              }),
+            });
+          }}
+        />
+      ),
+      className: (node.id === this.state.activeNote) ? 'active-tree-node' : '',
+      buttons: this._buildNodeButtons({ node, path }),
+      onClick: this.handleNodeClick.bind(this, node),
     });
   }
 
   render() {
-    console.log('rendering NoteLists........................');
+    console.log(`Active note: ${this.state.activeNote}`);
     return (
       <Tree
         className='tree'
         treeData={ this.state.notesTree }
         onChange={ this.handleChange }
-        generateNodeProps={ this.deleteNote }
+        generateNodeProps={ this.buildNodeProps }
       />
     );
   };
--- client_src/src/test/sample-tree.js
+++ client_src/src/test/sample-tree.js
@@ -1,3 +1,5 @@
+import uniqid from 'uniqid';
+
 function randomDateString(start, end) {
   return new Date(start.getTime() +
     Math.random() * (end.getTime() - start.getTime())).toLocaleString();
@@ -8,101 +10,129 @@ const end = new Date();
 
 export default [
   {
-    'title': 'react-ui-tree',
-    'subtitle': randomDateString(start, end),
-    'expanded': true,
-    'children': [
+    title: 'react-ui-tree',
+    subtitle: randomDateString(start, end),
+    id: uniqid(),
+    expanded: true,
+    children: [
       {
-        'title': 'dist',
-        'subtitle': randomDateString(start, end),
-        'children': [
+        title: 'dist',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
+        children: [
           {
-            'title': 'node.js',
-            'subtitle': randomDateString(start, end),
+            title: 'node.js',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
           },
           {
-            'title': 'react-ui-tree.css',
-            'subtitle': randomDateString(start, end),
+            title: 'react-ui-tree.css',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
           },
           {
-            'title': 'react-ui-tree.js',
-            'subtitle': randomDateString(start, end),
+            title: 'react-ui-tree.js',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
           },
           {
-            'title': 'tree.js',
-            'subtitle': randomDateString(start, end),
+            title: 'tree.js',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
           },
         ],
       },
       {
-        'title': 'example',
-        'subtitle': randomDateString(start, end),
-        'children': [
+        title: 'example',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
+        children: [
           {
-            'title': 'app.js',
-            'subtitle': randomDateString(start, end),
+            title: 'app.js',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
           },
           {
-            'title': 'app.less',
-            'subtitle': randomDateString(start, end),
+            title: 'app.less',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
           },
           {
-            'title': 'index.html',
-            'subtitle': randomDateString(start, end),
-          },
-        ],
-      },
-      {
-        'title': 'lib',
-        'subtitle': randomDateString(start, end),
-        'children': [
-          {
-            'title': 'node.js',
-            'subtitle': randomDateString(start, end),
-          },
-          {
-            'title': 'react-ui-tree.js',
-            'subtitle': randomDateString(start, end),
-          },
-          {
-            'title': 'react-ui-tree.less',
-            'subtitle': randomDateString(start, end),
-          },
-          {
-            'title': 'tree.js',
-            'subtitle': randomDateString(start, end),
+            title: 'index.html',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
           },
         ],
       },
       {
-        'title': '.gitiignore',
-        'subtitle': randomDateString(start, end),
+        title: 'lib',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
+        children: [
+          {
+            title: 'node.js',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
+          },
+          {
+            title: 'react-ui-tree.js',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
+          },
+          {
+            title: 'react-ui-tree.less',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
+          },
+          {
+            title: 'tree.js',
+            subtitle: randomDateString(start, end),
+            id: uniqid(),
+          },
+        ],
       },
       {
-        'title': 'index.js',
-        'subtitle': randomDateString(start, end),
+        title: '.gitiignore',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
       },
       {
-        'title': 'LICENSE',
-        'subtitle': randomDateString(start, end),
+        title: 'index.js',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
       },
       {
-        'title': 'Makefile',
-        'subtitle': randomDateString(start, end),
+        title: 'LICENSE',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
       },
       {
-        'title': 'package.json',
-        'subtitle': randomDateString(start, end),
+        title: 'Makefile',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
       },
       {
-        'title': 'README.md',
-        'subtitle': randomDateString(start, end),
+        title: 'package.json',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
       },
       {
-        'title': 'webpack.config.js',
-        'subtitle': randomDateString(start, end),
+        title: 'README.md',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
+      },
+      {
+        title: 'webpack.config.js',
+        subtitle: randomDateString(start, end),
+        id: uniqid(),
       },
     ],
+  },
+  {
+    title: 'react-sortable-tree bottom',
+    subtitle: randomDateString(start, end),
+    id: uniqid(),
+    children: [],
   },
 ];
 
