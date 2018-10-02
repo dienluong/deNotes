02eb2b40c873a9f737c83c1cfd56beda3115e35f
Dien Luong <dysonlu@gmail.com>
2018-10-01 23:13:27 -0400

    Fixed async setup of offlineStorage methods.

--- client_src/src/utils/offlineStorage.js
+++ client_src/src/utils/offlineStorage.js
@@ -2,47 +2,56 @@ import { create } from './remoteStorageJs';
 import notesDataModule from './notesDataModule';
 import treesDataModule from './treesDataModule';
 
-let _offlineStorage, _myExports;
-const storageMap = new Map();
+let _offlineStorage;
+let _save = () => Promise.reject('Not ready');
+let _load = () => Promise.reject('Not ready');
+let _remove = () => Promise.reject('Not ready');
+const privateStorageMap = new Map();
+
 try {
   _offlineStorage = create({ name: 'denotes' });
   _offlineStorage.addModule(notesDataModule);
   _offlineStorage.addModule(treesDataModule);
-  storageMap.set(notesDataModule.name, _offlineStorage[notesDataModule.name].privateContent());
-  storageMap.set(treesDataModule.name, _offlineStorage[treesDataModule.name].privateContent());
-}
-catch (err) {
+  privateStorageMap.set(notesDataModule.name, _offlineStorage[notesDataModule.name].privateContent());
+  privateStorageMap.set(treesDataModule.name, _offlineStorage[treesDataModule.name].privateContent());
+  _offlineStorage.on('ready', function produceMethods() {
+    _save = ({ collectionName, id, ownerId, dataObj }) => {
+      if (!privateStorageMap.has(collectionName)) {
+        return Promise.reject(new Error('Invalid collection'));
+      }
+
+      return privateStorageMap.get(collectionName).save({ id, ownerId, dataObj });
+    };
+
+    _load = ({ collectionName, id, ownerId }) => {
+      if (!privateStorageMap.has(collectionName)) {
+        return Promise.reject(new Error('Invalid collection'));
+      }
+
+      return privateStorageMap.get(collectionName).load({ id, ownerId });
+    };
+
+    _remove = ({ collectionName, ownerId, ids }) => {
+      if (!privateStorageMap.has(collectionName)) {
+        return Promise.reject(new Error('Invalid collection'));
+      }
+
+      return privateStorageMap.get(collectionName).destroy({ ownerId, ids });
+    };
+  });
+} catch (err) {
   // TODO: where to fallback if local storage is unavailable?
 }
 
-(async function waitForReady() {
-  _myExports = await new Promise(resolve => {
-    _offlineStorage.on('ready', function produceMethods() {
-      resolve({
-        _save({ collectionName, id, ownerId, dataObj }) {
-          if (!storageMap.has(collectionName)) {
-            return Promise.reject(new Error('Invalid collection'));
-          }
+export function save(paramsObj) {
+  return _save(paramsObj);
+}
 
-          return storageMap.get(collectionName).save({ id, ownerId, dataObj });
-        },
-        _load({ collectionName, id, ownerId }) {
-          if (!storageMap.has(collectionName)) {
-            return Promise.reject(new Error('Invalid collection'));
-          }
+export function load(paramsObj) {
+  return _load(paramsObj);
+}
 
-          return storageMap.get(collectionName).load({ id, ownerId });
-        },
-        _remove({ collectionName, ownerId, ids }) {
-          if (!storageMap.has(collectionName)) {
-            return Promise.reject(new Error('Invalid collection'));
-          }
+export function remove(paramsObj) {
+  return _remove(paramsObj);
+}
 
-          return storageMap.get(collectionName).destroy({ ownerId, ids });
-        },
-      });
-    });
-  });
-})();
-
-export const { _save: save, _load: load, _remove: remove } = _myExports;
