ad077a77e0128c9d095435837d1d935a29081d6b
Dien Luong <dysonlu@gmail.com>
2018-10-05 15:11:58 -0400

    Refactored the code that uses remoteStorageJS.

--- client_src/src/utils/remoteStorageJs/clientMixin.js
+++ client_src/src/utils/remoteStorageJs/clientMixin.js
@@ -1,54 +1,16 @@
-const moduleName = 'notes';
-
-function builder(privateClient) {
-  privateClient.declareType(moduleName, {
-    'type': 'object',
-    'properties': {
-      'id': {
-        'type': 'string',
-        'pattern': '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$',
-      },
-      'title': {
-        'type': 'string',
-      },
-      'content': {
-        'type': 'string',
-      },
-      'delta': {
-        'type': 'object',
-        'properties': {
-          'ops': {
-            'type': 'array',
-            'items': {
-              'type': 'object',
-            },
-          },
-        },
-      },
-      'dateCreated': {
-        'type': 'integer',
-      },
-      'dateModified': {
-        'type': 'integer',
-      },
-      'readOnly': {
-        'type': 'boolean',
-      },
-    },
-    'required': ['id'],
-  });
-
-  const notesDecorator = {
+const clientMixin = function clientMixin(moduleName) {
+  return {
     save({ ownerId, id, dataObj }) {
       const path = `${ownerId}/${id}`;
       return this.storeObject(moduleName, path, dataObj);
     },
+
     load({ ownerId, id }) {
       if (!id) {
         // getAll() is extremely poorly documented.
         // It returns an object containing all the objects stored.
         // The key is the object's path relative to the path provided to getAll().
-        // We are converting the object of objects into an array.
+        // We are converting the object of objects into an array of objects.
         return this.getAll(`${ownerId}/`)
           .then(data => {
             if (typeof data === 'object') {
@@ -74,7 +36,7 @@ function builder(privateClient) {
             return this.remove(`${ownerId}/${id}`)
               .then(() => id)
               .catch(caught => {
-                const error = new Error('Error deleting note: ' + id);
+                const error = new Error('Error while trying to delete. ID: ' + id);
                 error.caught = caught;
                 error.id = id;
                 return error;
@@ -89,20 +51,6 @@ function builder(privateClient) {
       }
     },
   };
-
-  const module = {
-    privateContent: () => Object.assign(privateClient, notesDecorator),
-  };
-
-  return {
-    exports: module,
-  };
-}
-
-
-const notesDataModule = {
-  name: moduleName,
-  builder: builder,
 };
 
-export default notesDataModule;
+export default clientMixin;
