67390d0ed57c31871e4926701fb234074448d37a
Dien Luong <dysonlu@gmail.com>
2018-11-09 13:13:06 -0500

    Fixed CSS classes

--- client_src/src/components/widgets/NotesList.js
+++ client_src/src/components/widgets/NotesList.js
@@ -77,7 +77,7 @@ function NotesList({
         onClick={ pathNavigatorClickHandler }
       />
       <Tree
-        className={ styles.dnt__tree }
+        className={ 'tree ' + styles.dnt__tree }
         treeData={ tree }
         onChange={ treeChangeHandler }
         getNodeKey={ getNodeKey }
