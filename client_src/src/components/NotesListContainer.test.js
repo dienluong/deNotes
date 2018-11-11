9b14afb5b8c1a9ab5f1c0e7e77d9bd235dcb0087
Dien Luong <dysonlu@gmail.com>
2018-11-10 22:39:37 -0500

    Added tests for NotesListContainer

--- client_src/src/components/NotesListContainer.test.js
+++ client_src/src/components/NotesListContainer.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import createMockStore from 'redux-mock-store';
+// import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { render, prettyDOM } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import 'jest-dom/extend-expect';
-import NotesListContainer from './NotesListContainer';
+import NotesListContainer, { TOOLBAR_LABELS } from './NotesListContainer';
 import NotesList from './widgets/NotesList';
 import initialState from '../redux/misc/initialState';
 import { mockedTree } from '../test-utils/mocks/mockedNotesTree';
@@ -24,13 +25,22 @@ import pathNavStyles from './widgets/PathNavigator.module.css';
 import notesListStyles from './widgets/NotesList.module.css';
 import nodeTitleStyles from './widgets/NodeTitle.module.css';
 
-import { configure, shallow } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
-let wrapper;
+let wrapper = {};
 
 afterEach(() => {
-  if (typeof wrapper.unmount === 'function') { wrapper.unmount(); }
+  if (typeof wrapper.unmount === 'function') {
+    wrapper.unmount();
+  }
+  cleanup();
+  addAndSelectNodeThunkAction.mockClear();
+  navigatePathAction.mockClear();
+  selectNodeThunkAction.mockClear();
+  changeNodeTitleAction.mockClear();
+  deleteNodeThunkAction.mockClear();
+  changeNotesTreeAction.mockClear();
 });
 
 it('render correctly with props with values taken from redux store', () => {
@@ -61,4 +71,73 @@ it('render correctly with props with values taken from redux store', () => {
 
   expect(wrapper.find(NotesList).exists()).toBe(true);
   expect(wrapper.find(NotesList).props()).toMatchObject(expectedProps);
+});
+
+it('react to events by calling proper handlers', () => {
+  const mockedNotesTree = { id: '1234', tree: mockedTree, dateCreated: 1111, dateModified: 2222 };
+  const mockedInitialState = {
+    ...initialState,
+    notesTree: mockedNotesTree,
+    activeNode: {
+      id: mockedNotesTree.tree[0].id,
+      path: [mockedNotesTree.tree[0].id],
+    },
+  };
+  addAndSelectNodeThunkAction.mockImplementation(() => ({ type: 'DUMMY_ACTION', payload: 'DUMMY_PAYLOAD' }));
+  navigatePathAction.mockImplementation(() => ({ type: 'DUMMY_ACTION', payload: 'DUMMY_PAYLOAD' }));
+  selectNodeThunkAction.mockImplementation(() => ({ type: 'DUMMY_ACTION', payload: 'DUMMY_PAYLOAD' }));
+  changeNodeTitleAction.mockImplementation(() => ({ type: 'DUMMY_ACTION', payload: 'DUMMY_PAYLOAD' }));
+  deleteNodeThunkAction.mockImplementation(() => ({ type: 'DUMMY_ACTION', payload: 'DUMMY_PAYLOAD' }));
+
+  const store = createMockStore([thunk])(mockedInitialState);
+  // const store = createStore((state) => state, mockedInitialState, applyMiddleware(thunk));
+
+  const { queryAllByText, container } = render(<Provider store={ store }><NotesListContainer /></Provider>);
+  // wrapper = mount(<Provider store={ store }><NotesListContainer /></Provider>);
+
+  // Test toolbar clicks
+  let target = queryAllByText(TOOLBAR_LABELS.NEW_FOLDER);
+  expect(target).toHaveLength(1);
+  fireEvent.click(target[0]);
+  // wrapper.find(`.${toolStyles.dnt__tool}[children="New Folder"]`).simulate('click');
+  expect(addAndSelectNodeThunkAction).toBeCalledTimes(1);
+
+  target = queryAllByText(TOOLBAR_LABELS.NEW_NOTE);
+  expect(target).toHaveLength(1);
+  fireEvent.click(target[0]);
+  expect(addAndSelectNodeThunkAction).toBeCalledTimes(2);
+  addAndSelectNodeThunkAction.mockClear();
+
+  // Test navbar click
+  target = container.querySelectorAll(`.${pathNavStyles["dnt__pathnav-segment--active"]}`);
+  expect(target).toHaveLength(1);
+  fireEvent.click(target[0]);
+  expect(navigatePathAction).toBeCalledTimes(1);
+
+  // Test node click
+  target = container.querySelectorAll(`.${nodeTitleStyles["dnt__node-title"]}`);
+  // our choosen mocked tree has more than one node
+  expect(target.length).toBeGreaterThan(1);
+  // arbitrarily click on second node
+  fireEvent.click(target[1]);
+  expect(selectNodeThunkAction).toBeCalledTimes(1);
+
+  // change the title of the second node
+  target[1].value = 'changed title';
+  fireEvent.blur(target[1]);
+  expect(changeNodeTitleAction).toBeCalledTimes(1);
+
+  target = queryAllByText('+');
+  // our mocked tree has more than one node
+  expect(target.length).toBeGreaterThan(1);
+  // just arbitrarily choose to click on '+' button of the second node
+  fireEvent.click(target[1]);
+  expect(addAndSelectNodeThunkAction).toBeCalledTimes(1);
+
+  target = queryAllByText('x');
+  // our mocked tree has more than one node
+  expect(target.length).toBeGreaterThan(1);
+  // just arbitrarily choose to click on '+' button of the second node
+  fireEvent.click(target[1]);
+  expect(deleteNodeThunkAction).toBeCalledTimes(1);
 });
