6c7822cb75a743855aa014d81b75e466548e2fc3
Dien Luong <dysonlu@gmail.com>
2018-11-14 12:07:01 -0500

    Minor change

--- client_src/src/App.test.js
+++ client_src/src/App.test.js
@@ -16,7 +16,7 @@ beforeAll(() => {
   };
 });
 
-it.skip('renders without crashing', () => {
+it('renders without crashing', () => {
   const store = createStore(() => initialState);
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={ store }><App /></Provider>, div);
