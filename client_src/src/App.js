afbf9d399a059b30ebe46c74660f9d45d5b88d2e
Dien Luong <dysonlu@gmail.com>
2019-03-30 20:21:52 -0400

    Installed Material UI globals

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -1,12 +1,16 @@
 // TODO: remove App.css
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { Component } from 'react';
 import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
-      <Main className="App" />
+      <React.Fragment>
+        <CssBaseline />
+        <Main className="App" />
+      </React.Fragment>
     );
   }
 }
