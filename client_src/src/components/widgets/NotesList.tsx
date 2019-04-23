f8789bc3ddce7fa6614768e105597591526fc232
Dien Luong <dysonlu@gmail.com>
2019-04-23 03:09:44 -0400

    Change: when in Root folder, only direct child nodes and folder nodes are rendered

--- client_src/src/components/widgets/NotesList.tsx
+++ client_src/src/components/widgets/NotesList.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Toolbar from './Toolbar';
 import PathNavigator from './PathNavigator';
+import NodeTitle from './NodeTitle';
 import Tree from 'react-sortable-tree';
+import { findDeepestFolder } from '../../utils/treeUtils';
 import mobileTheme from '../../tree-theme';
 import 'react-sortable-tree/style.css';
 import styles from './NotesList.module.css';
-import NodeTitle from './NodeTitle';
 import Typography from '@material-ui/core/Typography';
 import HomeIcon from '@material-ui/icons/Home';
 import NewFolderIcon from '@material-ui/icons/CreateNewFolder';
@@ -13,6 +14,7 @@ import NewNoteIcon from '@material-ui/icons/NoteAdd';
 
 // Types
 import { TreeItem } from 'react-sortable-tree';
+import { nodeTypes } from '../../utils/appCONSTANTS';
 type PropsT = {
   tree: TreeNodeT[],
   activeNode: ActiveNodeT,
@@ -45,13 +47,15 @@ export const TOOLBAR = {
   },
 };
 
+const _DEFAULT_FOLDER_NAME = '<NO_NAME>';
+const _DEFAULT_ROW_HEIGHT = 62;
+
 const dummyCb = () => {};
 const toolbarHandlersMap = new Map();
 toolbarHandlersMap.set(TOOLBAR.BACK_BUTTON, dummyCb)
                   .set(TOOLBAR.NEW_FOLDER, dummyCb)
                   .set(TOOLBAR.NEW_NOTE, dummyCb);
 const HandlersMapKeys = toolbarHandlersMap.keys();
-
 
 function NotesList({
   tree,
@@ -68,7 +72,7 @@ function NotesList({
   toolbarHandlers,
   getNodeKey,
 }: PropsT) {
-  function buildNodeProps({ node, path }: { node: TreeNodeT, path: string[] }) {
+  function buildNodeProps({ node, path }: { node: TreeNodeT, path: TreeNodePathT }) {
     return ({
       title: (<NodeTitle node={ node } path={ path } onSubmit={ nodeTitleChangeHandler } />),
       className: (node.id === activeNode.id) ? `${styles['dnt__tree-node']} ${styles['dnt__tree-node--active']}` : styles['dnt__tree-node'],
@@ -80,7 +84,27 @@ function NotesList({
     });
   }
 
-  function _buildNodeButtons({ node, path }: { node: TreeNodeT, path: string[] }) {
+  // make visible only folder nodes or nodes (of any type) in root
+  function buildRootFolderNodeProps({ node, path }: { node: TreeNodeT, path: TreeNodePathT }) {
+    if (node.type === nodeTypes.FOLDER || path.length === 1) {
+      return buildNodeProps({ node, path });
+    } else {
+      return ({
+        className: styles['dnt__tree-node--invisible'],
+      })
+    }
+  }
+
+  // give a height only to folder nodes or to nodes (of any type) in root
+  function RootFolderNodeRowHeight({ node, path }:{ node: TreeNodeT, path: TreeNodePathT }) {
+    if (node.type === nodeTypes.FOLDER || path.length === 1) {
+      return _DEFAULT_ROW_HEIGHT;
+    } else {
+      return 0;
+    }
+  }
+
+  function _buildNodeButtons({ node, path }: { node: TreeNodeT, path: TreeNodePathT }) {
     let buttons = [
       <button
         className={ styles['dnt__tree-node-btn'] }
@@ -118,6 +142,14 @@ function NotesList({
     }
   }
 
+  let generateNodeProps = buildNodeProps as generateNodePropsT;
+  let rowHeight: ((arg: any) => number) | number = _DEFAULT_ROW_HEIGHT;
+  // if current folder is root, then the tree will be rendered differently.
+  if (findDeepestFolder(activeNode.path) === -1) {
+    generateNodeProps = buildRootFolderNodeProps as generateNodePropsT;
+    rowHeight = RootFolderNodeRowHeight;
+  }
+
   return (
     <div className={ styles['dnt__notes-list'] }>
       <Toolbar toolsMap={ toolbarHandlersMap } />
@@ -127,7 +159,7 @@ function NotesList({
         onClick={ pathNavigatorClickHandler }
       />
       <Typography variant="h6" color="primary">
-        {folderName || '<NO_NAME>'}
+        { folderName || _DEFAULT_FOLDER_NAME }
       </Typography>
       <Tree
         className={ 'tree ' + styles.dnt__tree }
@@ -135,7 +167,9 @@ function NotesList({
         theme={ mobileTheme as any }
         onChange={ treeChangeHandler }
         getNodeKey={ getNodeKey }
-        generateNodeProps={ buildNodeProps as generateNodePropsT }
+        generateNodeProps={ generateNodeProps }
+        // @ts-ignore -- bug in react-sortable-tree/index.d.ts for rowHeight
+        rowHeight={ rowHeight }
       />
     </div>
   );
