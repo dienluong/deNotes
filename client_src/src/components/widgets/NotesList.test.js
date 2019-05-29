da8fb29fb893bdf0c5e43d22298cf11faad915d9
Dien Luong <dysonlu@gmail.com>
2019-05-29 16:57:25 -0400

    Tests: updated tests for NotesList

--- client_src/src/components/widgets/NotesList.test.js
+++ client_src/src/components/widgets/NotesList.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NotesList from './NotesList';
 import notesListStyles from './NotesList.module.css';
-import pathNavStyles from './PathNavigator.module.css';
 import AppBar from '@material-ui/core/AppBar';
 import MuiToolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -10,12 +9,8 @@ import HomeIcon from '@material-ui/icons/Home';
 import NewFolderIcon from '@material-ui/icons/CreateNewFolder';
 import NewNoteIcon from '@material-ui/icons/NoteAdd';
 import GoOutFolderIcon from '@material-ui/icons/ArrowBackIos';
-import GoInFolderIcon from '@material-ui/icons/ArrowForwardIos';
-import Tree, { walk, find, getFlatDataFromTree } from 'react-sortable-tree';
+import Tree, { find } from 'react-sortable-tree';
 import { getNodeKey, collapseFolders } from '../../utils/treeUtils';
-import { nodeTypes } from '../../utils/appCONSTANTS';
-import baseState from '../../redux/misc/initialState';
-import { selectTitlesFromActivePath } from '../../redux/selectors';
 import { mockedTree } from '../../test-utils/mocks/mockedNotesTree';
 import { render, cleanup, fireEvent, within } from 'react-testing-library';
 import 'jest-dom/extend-expect';
@@ -125,7 +120,7 @@ it('should display only folder nodes and all direct child nodes of root, in root
   const invisibleNodes = container.querySelectorAll('.dnt__tree-node--invisible');
   // In the mocked tree, we have strategically assigned titles to the nodes.
   // In this case, title starting w/ "note root0" means all notes that is children of node root0 (first child of root).
-  // We expect these to be the invisible nodes.
+  // We expect only these nodes to be invisible in root-view mode. Consequently, folder nodes and direct child nodes of root are visible.
   const check = text => expect(text).toEqual(expect.stringContaining('note root0'));
   invisibleNodes.forEach(node => {
     check(node.textContent);
@@ -133,36 +128,35 @@ it('should display only folder nodes and all direct child nodes of root, in root
 });
 
 it('should render all node titles of the received tree and highlight the active node', () => {
-  const tree = mockedTree;
-  // active node is an ITEM in root, so active folder is the root itself
+  // active node is an ITEM, so active folder is its parent; thus, rootViewOn is set to false
   const activeNode = {
-    id: mockedTree[1].id,
-    path: [mockedTree[1].id],
+    id: mockedTree[0].children[1].id,
+    path: [mockedTree[0].children[1].id],
   };
-  const activePath = selectTitlesFromActivePath({ ...baseState, notesTree: { ...baseState.notesTree, tree }, activeNode });
+  const tree = mockedTree[0].children;
   const props = {
     tree,
+    size: 'medium',
     activeNode,
-    activePath,
+    rootViewOn: false,
+    currentFolderName: 'TEST FOLDER NAME',
     treeChangeHandler: jest.fn(),
     nodeTitleChangeHandler: jest.fn(),
     nodeClickHandler: jest.fn(),
+    nodeDoubleClickHandler: jest.fn(),
     deleteNodeBtnHandler: jest.fn(),
-    addNoteBtnHandler: jest.fn(),
-    pathNavigatorClickHandler: jest.fn(),
-    toolbarHandlersMap,
-    toolbarHandlersMap2,
+    backBtnHandler: jest.fn(),
+    homeBtnHandler: jest.fn(),
+    toolbarHandlers: [jest.fn(), jest.fn()],
     getNodeKey,
   };
 
   const { container, queryAllByValue } = render(<NotesList { ...props }/>);
-
-  expect(container).toMatchSnapshot();
   const renderedInputs = Array.from(container.getElementsByTagName('input'));
   const renderedTitles = renderedInputs.map(input => input.value);
 
   const test = jest.fn();
-  test.mockImplementation(({ node }) => {
+  test.mockImplementation(node => {
     if (node.id === activeNode.id) {
       const highlightedNodes = container.getElementsByClassName(notesListStyles['dnt__tree-node--active']);
       // should expect only one highlighted node
@@ -174,44 +168,39 @@ it('should render all node titles of the received tree and highlight the active 
   });
 
   // For each node in the tree sent to NotesList component, run test.
-  walk({
-    treeData: props.tree,
-    getNodeKey,
-    callback: test,
-    ignoreCollapsed: true,
-  });
+  props.tree.forEach(test);
 
-  // The current active folder (the root) has total of 9 children and grandchildren nodes
-  expect(test).toBeCalledTimes(9);
+  // The current active folder has total of 4 children nodes
+  expect(test).toBeCalledTimes(4);
+  const numOfNodesInSourceTree = props.tree.length;
   const numOfRenderedTreeNodeElements = container.getElementsByClassName(notesListStyles['dnt__tree-node']).length;
-  const numOfNodesInSourceTree = getFlatDataFromTree({ treeData: props.tree, getNodeKey, ignoreCollapsed: true }).length;
   expect(numOfNodesInSourceTree).toEqual(numOfRenderedTreeNodeElements);
 });
 
 it('should invoke handler when tree node is clicked', () => {
-  const tree = mockedTree;
+  // selected node is an note in root folder
   const activeNode = {
     id: mockedTree[1].id,
     path: [mockedTree[1].id],
   };
-  const activePath = selectTitlesFromActivePath({ ...baseState, notesTree: { ...baseState.notesTree, tree }, activeNode });
   const props = {
-    tree,
+    tree: mockedTree,
+    size: 'medium',
     activeNode,
-    activePath,
+    rootViewOn: true,
+    currentFolderName: 'TEST FOLDER NAME',
     treeChangeHandler: jest.fn(),
     nodeTitleChangeHandler: jest.fn(),
     nodeClickHandler: jest.fn(),
+    nodeDoubleClickHandler: jest.fn(),
     deleteNodeBtnHandler: jest.fn(),
-    addNoteBtnHandler: jest.fn(),
-    pathNavigatorClickHandler: jest.fn(),
-    toolbarHandlersMap,
-    toolbarHandlersMap2,
+    backBtnHandler: jest.fn(),
+    homeBtnHandler: jest.fn(),
+    toolbarHandlers: [jest.fn(), jest.fn()],
     getNodeKey,
   };
 
   const { container } = render(<NotesList { ...props }/>);
-
   const renderedTreeNodeElements = [...container.getElementsByClassName(notesListStyles['dnt__tree-node'])];
   renderedTreeNodeElements.forEach(element => {
     const elementTitle = element.getElementsByTagName('input')[0].value;
@@ -229,35 +218,34 @@ it('should invoke handler when tree node is clicked', () => {
     expect(props.nodeClickHandler).lastCalledWith({ id: correspondingNode[0].node.id, path: correspondingNode[0].path });
   });
 
-  const numOfNodesInSourceTree = getFlatDataFromTree({ treeData: props.tree, getNodeKey, ignoreCollapsed: true }).length;
-  expect(props.nodeClickHandler).toBeCalledTimes(numOfNodesInSourceTree);
+  // All folder nodes and notes nodes in root of the mocked tree should have been clicked.
+  expect(props.nodeClickHandler).toBeCalledTimes(5);
 });
 
 it('should invoke handler when double-click on node', () => {
-  const tree = mockedTree;
+  // selected node is a folder in root; so that folder's content is displayed, not the root's; therefore rootViewOn is false
   const activeNode = {
-    id: mockedTree[1].id,
-    path: [mockedTree[1].id],
+    id: mockedTree[0].id,
+    path: [mockedTree[0].id],
   };
-  const activePath = selectTitlesFromActivePath({ ...baseState, notesTree: { ...baseState.notesTree, tree }, activeNode });
   const props = {
-    tree,
+    tree: mockedTree[0].children,
+    size: 'medium',
     activeNode,
-    activePath,
+    rootViewOn: false,
+    currentFolderName: 'TEST FOLDER NAME',
     treeChangeHandler: jest.fn(),
     nodeTitleChangeHandler: jest.fn(),
     nodeClickHandler: jest.fn(),
     nodeDoubleClickHandler: jest.fn(),
     deleteNodeBtnHandler: jest.fn(),
-    addNoteBtnHandler: jest.fn(),
-    pathNavigatorClickHandler: jest.fn(),
-    toolbarHandlersMap,
-    toolbarHandlersMap2,
+    backBtnHandler: jest.fn(),
+    homeBtnHandler: jest.fn(),
+    toolbarHandlers: [jest.fn(), jest.fn()],
     getNodeKey,
   };
 
   const { container } = render(<NotesList { ...props }/>);
-
   const renderedTreeNodeElements = [...container.getElementsByClassName(notesListStyles['dnt__tree-node'])];
   renderedTreeNodeElements.forEach(element => {
     const elementTitle = element.getElementsByTagName('input')[0].value;
@@ -275,188 +263,123 @@ it('should invoke handler when double-click on node', () => {
     expect(props.nodeDoubleClickHandler).lastCalledWith({ id: correspondingNode[0].node.id, path: correspondingNode[0].path });
   });
 
-  const numOfNodesInSourceTree = getFlatDataFromTree({ treeData: props.tree, getNodeKey, ignoreCollapsed: true }).length;
-  expect(props.nodeDoubleClickHandler).toBeCalledTimes(numOfNodesInSourceTree);
+
+  expect(props.nodeDoubleClickHandler).toBeCalledTimes(props.tree.length);
 });
 
-it('should invoke handler when toolbar button is clicked', () => {
-  const tree = mockedTree;
+it('should invoke handler when buttons on toolbars are clicked', () => {
+  // active node is a FOLDER in root, so active folder is that FOLDER node (and not the root); thus, rootViewOn is set to false
   const activeNode = {
     id: mockedTree[0].id,
     path: [mockedTree[0].id],
   };
-  const activePath = selectTitlesFromActivePath({ ...baseState, notesTree: { ...baseState.notesTree, tree }, activeNode });
+  const tree = mockedTree[0].children;
   const props = {
     tree,
+    size: 'small',
     activeNode,
-    activePath,
+    rootViewOn: false,
+    currentFolderName: 'TEST FOLDER NAME',
     treeChangeHandler: jest.fn(),
     nodeTitleChangeHandler: jest.fn(),
     nodeClickHandler: jest.fn(),
+    nodeDoubleClickHandler: jest.fn(),
     deleteNodeBtnHandler: jest.fn(),
-    addNoteBtnHandler: jest.fn(),
-    pathNavigatorClickHandler: jest.fn(),
-    toolbarHandlersMap,
-    toolbarHandlersMap2,
+    backBtnHandler: jest.fn(),
+    homeBtnHandler: jest.fn(),
+    toolbarHandlers: [jest.fn(), jest.fn()],
     getNodeKey,
   };
 
-  const { queryAllByText } = render(<NotesList { ...props }/>);
+  wrapper = shallow(<NotesList { ...props } />);
 
-  for (let [label, handler] of toolbarHandlersMap) {
-    const button = queryAllByText(label);
-    expect(button).toHaveLength(1);
-    fireEvent.click(button[0]);
-    expect(handler).toBeCalled();
-  }
-
-  for (let [label, handler] of toolbarHandlersMap2) {
-    const button = queryAllByText(label);
-    expect(button).toHaveLength(1);
-    fireEvent.click(button[0]);
-    expect(handler).toBeCalled();
-  }
-
-  toolbarHandlersMap.forEach(handler => expect(handler).toBeCalledTimes(1));
-  toolbarHandlersMap2.forEach(handler => expect(handler).toBeCalledTimes(1));
+  wrapper.find(IconButton).forEach(button => button.simulate('click'));
+  expect(props.backBtnHandler).toBeCalledTimes(1);
+  expect(props.homeBtnHandler).toBeCalledTimes(1);
+  props.toolbarHandlers.forEach(handler => expect(handler).toBeCalledTimes(1));
 });
 
-it('should invoke handler when segment in path navigator is clicked', () => {
-  const tree = mockedTree;
+it('should invoke handler when tree node delete button is clicked', () => {
   const activeNode = {
-    id: mockedTree[0].id,
-    path: [mockedTree[0].id],
+    id: mockedTree[0].children[0].id,
+    path: [mockedTree[0].children[0].id],
   };
-  const activePath = selectTitlesFromActivePath({ ...baseState, notesTree: { ...baseState.notesTree, tree }, activeNode });
   const props = {
-    tree,
+    tree: mockedTree[0].children,
+    size: 'medium',
     activeNode,
-    activePath,
+    rootViewOn: false,
+    currentFolderName: 'TEST FOLDER NAME',
     treeChangeHandler: jest.fn(),
     nodeTitleChangeHandler: jest.fn(),
     nodeClickHandler: jest.fn(),
+    nodeDoubleClickHandler: jest.fn(),
     deleteNodeBtnHandler: jest.fn(),
-    addNoteBtnHandler: jest.fn(),
-    pathNavigatorClickHandler: jest.fn(),
-    toolbarHandlersMap,
-    toolbarHandlersMap2,
-    getNodeKey,
-  };
-
-  const { container } = render(<NotesList { ...props }/>);
-  expect(container.getElementsByClassName(pathNavStyles.dnt__pathnav)).toHaveLength(1);
-  const renderedPathNav = container.getElementsByClassName(pathNavStyles.dnt__pathnav).item(0);
-  activePath.forEach((segment, idx) => {
-    const renderedPathSegment = within(renderedPathNav).getByText(new RegExp(`${segment}`));
-    fireEvent.click(renderedPathSegment);
-    expect(props.pathNavigatorClickHandler).lastCalledWith({ idx });
-    expect(props.pathNavigatorClickHandler).toBeCalledTimes(1);
-  });
-});
-
-it('should invoke handler when tree node button is clicked', () => {
-  const tree = mockedTree;
-  const activeNode = {
-    id: mockedTree[1].id,
-    path: [mockedTree[1].id],
-  };
-  const activePath = selectTitlesFromActivePath({ ...baseState, notesTree: { ...baseState.notesTree, tree }, activeNode });
-  const props = {
-    tree,
-    activeNode,
-    activePath,
-    treeChangeHandler: jest.fn(),
-    nodeTitleChangeHandler: jest.fn(),
-    nodeClickHandler: jest.fn(),
-    deleteNodeBtnHandler: jest.fn(),
-    addNoteBtnHandler: jest.fn(),
-    pathNavigatorClickHandler: jest.fn(),
-    toolbarHandlersMap,
-    toolbarHandlersMap2,
+    backBtnHandler: jest.fn(),
+    homeBtnHandler: jest.fn(),
+    toolbarHandlers: [jest.fn(), jest.fn()],
     getNodeKey,
   };
 
   const { getByTestId } = render(<NotesList { ...props }/>);
 
-  let numberOfFolders = 0;
-
+  let nodeCounter = 0;
   const test = jest.fn();
-  test.mockImplementation(({ node, path }) => {
-    if (node.type === nodeTypes.FOLDER) {
-      numberOfFolders += 1;
-      const renderedTreeNode = getByTestId(node.id);
-      const renderedTitles = within(renderedTreeNode).queryAllByValue(node.title);
-      expect(renderedTitles).toHaveLength(1);
-      const nodeAddButton = within(renderedTreeNode).getByText('+');
-      const nodeDeleteButton = within(renderedTreeNode).getByText('x');
+  test.mockImplementation(node => {
+    nodeCounter += 1;
+    const renderedTreeNode = getByTestId(node.id);
+    const renderedTitles = within(renderedTreeNode).queryAllByValue(node.title);
+    expect(renderedTitles).toHaveLength(1);
+    const nodeDeleteButton = within(renderedTreeNode).getByText('x');
 
-      fireEvent.click(nodeAddButton);
-      expect(props.addNoteBtnHandler).lastCalledWith({ path });
-      expect(props.addNoteBtnHandler).toBeCalledTimes(numberOfFolders);
-
-      fireEvent.click(nodeDeleteButton);
-      expect(props.deleteNodeBtnHandler).lastCalledWith({ node, path });
-      expect(props.deleteNodeBtnHandler).toBeCalledTimes(numberOfFolders);
-    }
+    fireEvent.click(nodeDeleteButton);
+    expect(props.deleteNodeBtnHandler).lastCalledWith({ node: expect.any(Object), path: expect.any(Array) });
+    expect(props.deleteNodeBtnHandler).toBeCalledTimes(nodeCounter);
   });
 
-  walk({
-    treeData: props.tree,
-    getNodeKey,
-    callback: test,
-    ignoreCollapsed: true,
-  });
-
-  // There is a total of 9 nodes
-  expect(test).toBeCalledTimes(9);
+  props.tree.forEach(test);
+  expect(test).toBeCalledTimes(props.tree.length);
 });
 
 it('should invoke handler on node title change', () => {
-  const tree = mockedTree;
   const activeNode = {
-    id: mockedTree[1].id,
-    path: [mockedTree[1].id],
+    id: mockedTree[0].children[0].id,
+    path: [mockedTree[0].children[0].id],
   };
-  const activePath = selectTitlesFromActivePath({ ...baseState, notesTree: { ...baseState.notesTree, tree }, activeNode });
   const props = {
-    tree,
+    tree: mockedTree[0].children,
+    size: 'medium',
     activeNode,
-    activePath,
+    rootViewOn: false,
+    currentFolderName: 'TEST FOLDER NAME',
     treeChangeHandler: jest.fn(),
     nodeTitleChangeHandler: jest.fn(),
     nodeClickHandler: jest.fn(),
+    nodeDoubleClickHandler: jest.fn(),
     deleteNodeBtnHandler: jest.fn(),
-    addNoteBtnHandler: jest.fn(),
-    pathNavigatorClickHandler: jest.fn(),
-    toolbarHandlersMap,
-    toolbarHandlersMap2,
+    backBtnHandler: jest.fn(),
+    homeBtnHandler: jest.fn(),
+    toolbarHandlers: [jest.fn(), jest.fn()],
     getNodeKey,
   };
 
   const { queryAllByValue } = render(<NotesList { ...props }/>);
 
-  let numberOfNodes = 0;
+  let nodeCounter = 0;
   const test = jest.fn();
-  test.mockImplementation(({ node, path }) => {
+  test.mockImplementation(node => {
     const newTitle = 'new test title';
     const renderedInputs = queryAllByValue(node.title);
     expect(renderedInputs).toHaveLength(1);
     const renderedTitle = renderedInputs[0];
 
-    numberOfNodes += 1;
+    nodeCounter += 1;
     renderedTitle.value = newTitle;
     fireEvent.blur(renderedTitle);
-    expect(props.nodeTitleChangeHandler).lastCalledWith({ node, path, title: newTitle });
-    expect(props.nodeTitleChangeHandler).toBeCalledTimes(numberOfNodes);
+    expect(props.nodeTitleChangeHandler).lastCalledWith({ node: expect.any(Object), path: expect.any(Array), title: newTitle });
+    expect(props.nodeTitleChangeHandler).toBeCalledTimes(nodeCounter);
   });
 
-  walk({
-    treeData: props.tree,
-    getNodeKey,
-    callback: test,
-    ignoreCollapsed: true,
-  });
-
-  // There is a total of 9 nodes.
-  expect(test).toBeCalledTimes(9);
+  props.tree.forEach(test);
+  expect(test).toBeCalledTimes(props.tree.length);
 });
