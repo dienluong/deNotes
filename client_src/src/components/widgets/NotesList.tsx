51f4e1ba00883352c4c9d0fc152ba85cbf0b6c01
Dien Luong <dysonlu@gmail.com>
2019-08-01 16:26:07 -0400

    Change: default cursor for folder name in noteslist header

--- client_src/src/components/widgets/NotesList.tsx
+++ client_src/src/components/widgets/NotesList.tsx
@@ -152,7 +152,7 @@ function NotesList({
               <ExitFolderIcon />
             </IconButton>
           )}
-          <Typography inline variant={ size !== 'small' ? 'h5' : 'h6' } color="inherit">
+          <Typography inline variant={ size !== 'small' ? 'h5' : 'h6' } color="inherit" style={{ cursor: 'default' }}>
             { unescape(currentFolderName || _DEFAULT_FOLDER_NAME) }
           </Typography>
           { !rootViewOn && !editMode && (
