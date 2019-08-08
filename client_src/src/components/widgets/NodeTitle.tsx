55c3b7e38ac55db91e634bd63b30f7e200412916
Dien Luong <dysonlu@gmail.com>
2019-08-08 01:49:38 -0400

    New: title input blurs on submit

--- client_src/src/components/widgets/NodeTitle.tsx
+++ client_src/src/components/widgets/NodeTitle.tsx
@@ -17,6 +17,7 @@ function NodeTitle({ node, path, onSubmit: submitHandler }: PropsT<TreeNodeT, an
     // but event.currentTarget is *not* <input>, but rather <form>, if the event was onSubmit
     const inputEl = event.currentTarget.matches('input') ? event.currentTarget : event.currentTarget.getElementsByTagName('input')[0];
     if (inputEl instanceof HTMLInputElement) {
+      inputEl.blur();
       const escapedInput = escape(inputEl.value);
       if (escapedInput !== inputEl.defaultValue) {
         submitHandler({
@@ -37,6 +38,7 @@ function NodeTitle({ node, path, onSubmit: submitHandler }: PropsT<TreeNodeT, an
         defaultValue={ unescape(node.title) }
         onBlur={ submit }
         onClick={ event => event.stopPropagation() }
+        onDoubleClick={ event => event.stopPropagation() }
       />
     </form>
   );
